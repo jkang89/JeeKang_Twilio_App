04523cc96a0e54e1a52d6d11b44e3ee6ba8a42ad
Jee Kang <jeekang908@gmail.com>
2019-03-09 23:44:54 -0800

    Add in pseudocode on next steps

--- script.py
+++ script.py
@@ -1,2 +1,7 @@
-while True:
-	print("I am working!")
+# Import from Twilio and Beautiful Soup
+
+# Denote r/WhatsWrongWithYourDog as the page to scrape
+# Use Beautiful Soup to grab top 3 titles and URLs from subreddit
+# Integrate Twilio to send list of top 3 posts as text
+# Deploy to Heroku and automate sending of text daily
+
